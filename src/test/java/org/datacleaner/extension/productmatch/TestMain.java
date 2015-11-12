04d0c3619c83aea109425c2a02cfe87e68bc0c06
ClaudiaPHI <claudia.pesu@humaninference.com>
2015-11-12 21:27:37 +0100

    Added image to the extension Fixes #3

---
+++ src/test/java/org/datacleaner/extension/productmatch/TestMain.java
@@ -0,0 +1,12 @@
+package org.datacleaner.extension.productmatch;
+
+import org.datacleaner.Main;
+
+
+public class TestMain {
+
+    public static void main(final String[] args) {
+        Main.main(args);
+    }
+    
+}
