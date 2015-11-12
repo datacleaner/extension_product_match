7dfd455486a4e7bed69273694ff4320dc9a30778
ClaudiaPHI <claudia.pesu@humaninference.com>
2015-11-12 13:56:54 +0100

    projetc setup

---
+++ product_match/extension_product_match/ProductMatchTransformer.java
@@ -0,0 +1,25 @@
+package extension_product_match;
+
+import org.datacleaner.api.InputRow;
+import org.datacleaner.api.OutputColumns;
+import org.datacleaner.api.Transformer;
+
+public class ProductMatchTransformer implements Transformer {
+
+    
+    
+    
+    
+    @Override
+    public OutputColumns getOutputColumns() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object[] transform(InputRow arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
